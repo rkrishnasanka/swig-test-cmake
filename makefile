9b3fa2fccd801ea1b293bfac25e32d3de1027f9c
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-07-15 22:53:51 -0400

    Cleaned up for CMake

--- makefile
+++
@@ -1,7 +0,0 @@
-main: swig python_dist
-
-python_dist:
-	python3 setup.py build_ext --inplace
-
-swig:
-	swig -c++ -python example.i
--- test.py
+++ test.py
@@ -13,4 +13,22 @@ print(ret[0])
 for i in ret:
     print(i)
 
+s = Shape()
+s.area()
+s.move(5,6)
 
+pass_print_vector([1,5,9])
+
+arr = []
+
+arr.append(56)
+arr.append(65)
+arr.append(99)
+arr.append(700)
+
+pass_print_vector(arr)
+
+s2 = Shape(69, 666)
+s2.print_shape()
+
+Shape.print_shape_vector([s, s2])
